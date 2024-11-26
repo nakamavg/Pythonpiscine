8196630dce08193fc8ad169ddddade75df3941a1
Nakamavg <aecm.davidgomez@gmail.com>
2024-11-26 15:27:42 +0100

    isinstance

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -1,26 +1,28 @@
 def all_thing_is_obj(obj: any) -> int:
-	"""
-	Imprime el tipo del objeto recibido y retorna 42.
-	
-	Args:
-		obj: Cualquier objeto en Python.
-		
-	Returns:
-		int: Retorna siempre el valor 42.
-	"""
-	type_messages = {
-		list: "List",
-		tuple: "Tuple",
-		set: "Set",
-		dict: "Dict",
-		str: f"{obj} is in the kitchen"
-	}
-	
-	message = type_messages.get(type(obj), "Type not found")
-	print(f"{message} : {type(obj)}" if message != "Type not found" else message)
-	
-	return 42
+    """
+    Imprime el tipo del objeto recibido y retorna 42.
+    
+    Args:
+        obj: Cualquier objeto en Python.
+        
+    Returns:
+        int: Retorna siempre el valor 42.
+    """
+    if isinstance(obj, list):
+        print(f"List : {type(obj)}")
+    elif isinstance(obj, tuple):
+        print(f"Tuple : {type(obj)}")
+    elif isinstance(obj, set):
+        print(f"Set : {type(obj)}")
+    elif isinstance(obj, dict):
+        print(f"Dict : {type(obj)}")
+    elif isinstance(obj, str):
+        print(f"{obj} is in the kitchen : {type(obj)}")
+    else:
+        print("Type not found")
+    return 42
+
 
 if __name__ == "__main__":
-	# Este bloque se asegura de que no haga nada si es llamado directamente.
-	pass
+    # Este bloque se asegura de que no haga nada si es llamado directamente.
+    pass
