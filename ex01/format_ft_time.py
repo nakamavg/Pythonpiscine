16867f8d50b33d0495cfc18eb3a4880ac7c81179
Nakamavg <aecm.davidgomez@gmail.com>
2024-11-26 12:10:26 +0100

    fix data

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,14 +1,12 @@
 import time
 from datetime import datetime, timezone
 
-# Obtener el tiempo en segundos desde Epoch (1 de enero de 1970)
+# Obtener el tiempo actual
 current_time = time.time()
 
-# Imprimir en formato decimal completo
-#.4f indica que se imprimirán 4 decimales
-#.2e indica que se imprimirá en notación científica con 2 decimales
-print(f"Seconds since January 1, 1970: {current_time:.4f} or {current_time:.2e} in scientific notation")
+# Imprimir los segundos desde el Epoch con 4 decimales y en notación científica
+print(f"Seconds since January 1, 1970: {current_time:.4f} or {current_time:.2e} in scientific notation$")
 
-# Formatear fecha y hora legibles usando objetos de fecha y hora con conocimiento de zona horaria
-formatted_time = datetime.fromtimestamp(current_time, tz=timezone.utc).strftime('%b %d')
-print(formatted_time)
+# Formatear la fecha como "Nov 26" usando un objeto timezone-aware
+formatted_date = datetime.fromtimestamp(current_time, tz=timezone.utc).strftime('%b %d')
+print(formatted_date)
