cf67beea0656e6cb3c985a82b6d4d07e1a66c84f
Nakamavg <aecm.davidgomez@gmail.com>
2024-11-26 16:18:00 +0100

    i have a date in decimal and scients anotation

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -2,13 +2,13 @@ import time
 from datetime import datetime, timezone
 
 # Obtener el tiempo actual
-
-decimal_anotation = "{:,.4f}".format(time.time())
-scientific_anotation = "{:.2e}".format(time.time())
+current_time = time.time()
+decimal_anotation = "{:,.4f}".format(current_time)
+scientific_anotation = "{:.2e}".format(current_time)
 
 # Imprimir los segundos desde el Epoch con 4 decimales y en notación científica
 print(f"Seconds since January 1, 1970: {decimal_anotation} or {scientific_anotation} in scientific notation$")
 
 # Formatear la fecha como "Nov 26" usando un objeto timezone-aware
-formatted_date = datetime.fromtimestamp(time.time(), tz=timezone.utc).strftime('%b %d')
+formatted_date = datetime.fromtimestamp(current_time, tz=timezone.utc).strftime('%b %d')
 print(formatted_date)
